fd73a00b4d9d8df68a88237f56d6a1d0d4035bab
angrwl <43722764+angrwl@users.noreply.github.com>
2018-11-18 11:54:34 +0000

    This is our final version - i have added many pop-up windows

--- popup.js
+++ popup.js
@@ -3,3 +3,21 @@ document.getElementById('button').addEventListener('click', function(){
     
     document.getElementById('box').style.display='block';
 });
+
+document.getElementById('button1').addEventListener('click', function () {
+    console.log('submit');
+
+    document.getElementById('submit_box').style.display = 'block';
+});
+
+document.getElementById('button2').addEventListener('click', function () {
+    console.log('share_button');
+
+    document.getElementById('share_box').style.display = 'block';
+});
+
+document.getElementById('button3').addEventListener('click', function () {
+    console.log('help_button');
+
+    document.getElementById('help_box').style.display = 'block';
+});
